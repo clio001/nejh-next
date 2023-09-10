bc11bab41f6dbc5beb2b1df8890dcd42e56870d4
clio001 <johnwoitkowitz@hotmail.com>
2023-09-10 15:54:57 +0200

    fixed bootstrap navbar

--- chs-next/comps/Navbar.js
+++ chs-next/comps/Navbar.js
@@ -1,85 +1,74 @@
-import React, { useContext } from "react";
-import { useRouter } from "next/router";
-import {
-  Typography,
-  Box,
-  TextField,
-  Button,
-  Modal,
-  Drawer,
-} from "@mui/material";
-import Link from "next/link";
-import styles from "../styles/Navbar.module.css";
-import { uiContext } from "../context/uiContext";
-import BrowserPanel from "./BrowserPanel";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Box } from "@mui/material";
 
-function Navbar() {
-  const { openPanel, setOpenPanel } = useContext(uiContext);
-  const router = useRouter();
+function Navi() {
   return (
-    <div>
-      <Box className={styles.navbar_container}>
-        <Link href="/">
-          <a>
+    <>
+      <Navbar
+        style={{ fontFamily: "Palatino", color: "#FF0000" }}
+        expand="lg"
+        className="bg-body-tertiary"
+      >
+        <Container>
+          <Navbar.Brand
+            href="/"
+            style={{ display: "flex", flexDirection: "row" }}
+          >
             <img
               src="https://static.wixstatic.com/media/725a03_e741d97a0fa64d699b1ee3166ef58aeb.jpeg"
               alt="Logo of the New England Journal of History"
-              height="75rem"
+              height="80rem"
+              style={{ marginTop: "0.4rem", marginRight: "1rem" }}
             />
-          </a>
-        </Link>
-        <Typography
-          variant="body1"
-          ml={1}
-          mr={3}
-          sx={{ fontFamily: "Palatino" }}
-        >
-          CITIZEN
-          <br />
-          HISTORIAN
-          <br />
-          SOURCEBOOK
-        </Typography>{" "}
-        <Button
-          variant="contained"
-          className={styles.button}
-          onClick={() => router.push("https://www.nejh.org")}
-        >
-          NEJH
-        </Button>
-        <Button
-          variant="contained"
-          className={styles.button}
-          onClick={() => router.push("https://www.nejh.org/current-issue")}
-        >
-          Current Issue
-        </Button>{" "}
-        <Button
-          variant="contained"
-          className={styles.button}
-          onClick={() => router.push("https://www.nejh.org/subscribe")}
-        >
-          Subscribe
-        </Button>{" "}
-        <Button
-          variant="contained"
-          className={styles.button}
-          onClick={() => router.push("https://www.nejh.org/citizen-historians")}
-        >
-          Citizen Historian
-        </Button>{" "}
-      </Box>
-      <Drawer
-        anchor="top"
-        variant="temporary"
-        open={openPanel}
-        onClose={() => setOpenPanel(false)}
-        className="panel-modal"
-      >
-        <BrowserPanel />
-      </Drawer>
-    </div>
+            <Box>
+              CITIZEN
+              <br />
+              HISTORIAN
+              <br />
+              SOURCEBOOK
+            </Box>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="https://www.nejh.org"
+                target="_blank"
+                style={{ fontSize: "20px" }}
+              >
+                NEJH
+              </Nav.Link>
+              <Nav.Link
+                href="https://www.nejh.org/current-issue"
+                target="_blank"
+                style={{ fontSize: "20px" }}
+              >
+                Current Issue
+              </Nav.Link>
+
+              <Nav.Link
+                href="https://www.nejh.org/subscribe"
+                target="_blank"
+                style={{ fontSize: "20px" }}
+              >
+                Subscribe
+              </Nav.Link>
+              <Nav.Link
+                href="https://www.nejh.org/citizen-historians"
+                target="_blank"
+                style={{ fontSize: "20px" }}
+              >
+                Citizen Historian
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
 
-export default Navbar;
+export default Navi;
